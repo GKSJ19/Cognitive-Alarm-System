7667fd31f9ef4f4cdea86b9cb2506de1aba8f912
Nivedhitha8 <nivedhitha378@gmail.com>
2026-08-20 19:12:25 +0530

    Add files via upload

---
+++ darkTheme.ts
@@ -0,0 +1,25 @@
+import Colors from "./colors";
+
+const DarkTheme = {
+  mode: "dark",
+
+  colors: {
+    background: Colors.dark.background,
+    surface: Colors.dark.surface,
+
+    primary: Colors.primary,
+    secondary: Colors.secondary,
+
+    text: Colors.dark.text,
+    textSecondary: Colors.dark.textSecondary,
+
+    border: Colors.dark.border,
+    card: Colors.dark.card,
+
+    success: Colors.success,
+    warning: Colors.warning,
+    error: Colors.error,
+  },
+};
+
+export default DarkTheme;
